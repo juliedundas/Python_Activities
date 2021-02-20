3b0ef36d42b3492f6008fed740ec2ad951931b00
juliedundas <juliemdundas@gmail.com>
2021-02-20 16:51:01 -0700

    Added netflix practice to repo

--- README.md
+++ README.md
@@ -3,3 +3,26 @@ I have created this repository to include various applications that I created wi
 
 # First Activity:
 - Rock, Paper, Scissors
+
+## Instructions
+
+* Using the terminal, take an input of `r`, `p` or `s` which will stand for rock, paper, and scissors.
+
+* Have the computer randomly pick one of these three choices.
+
+* Compare the user's input to the computer's choice to determine if the user won, lost, or tied.
+
+# Second Activity:
+- Read Netflix
+
+## Instructions
+
+* Prompt the user for what video they are looking for.
+
+* Search through the `netflix_ratings.csv` to find the user's video.
+
+* If the CSV contains the user's video then print out the title, what it is rated and the current user ratings.
+
+  * For example "Pup Star is rated G with a rating of 82"
+
+* If the CSV does not contain the user's video then print out a message telling them that their video could not be found.
---
+++ netflix.py
@@ -0,0 +1,31 @@
+#Import modules
+import os
+import csv
+
+#Prompt the user for what video they are looking for
+video = input("Which program are you looking for? ").title()
+
+#Set path for csv file
+csvpath = os.path.join("Resources", "netflix_ratings.csv")
+
+#Set variable to check if program is on list
+exists = False
+
+#Open the csv
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",") #programs separated by commas in csv file
+
+    #Loop through the file to locate the desired program
+    for row in csvreader:
+        if row[0] == video:
+            print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
+
+            #Set variable to confirm is program is listed
+            exists = True
+
+            #Stop running code if one exists so that no duplicates are returned
+            break
+
+    #If the video does not exist, notify the requester
+    if exists is False:
+        print("We do not have what you are requesting. Please try another title.")    
